6a048199601c86d02a6f682ac8cb03f6abe8984f
Vikram Parashar <vikramparashar24@gmail.com>
2025-10-08 00:45:24 +0530

    switched to lazy pkg manger in neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,7 +5,6 @@ require("config.set")
 vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
 	callback = function()
 		if vim.bo.modified and vim.bo.buftype == "" then
-			require("conform").format({ async = false, lsp_fallback = true })
 			vim.cmd("silent! write")
 		end
 	end,
